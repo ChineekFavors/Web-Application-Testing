0a73fa3370c0909922eaa949a6c6ac765eee53f5
Chineek Favors <chineekfavors01@gmail.com>
2019-09-12 20:29:15 -0700

    added state to app.js

--- web-app-testing/src/App.js
+++ web-app-testing/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import './App.css';
 
@@ -6,10 +6,13 @@ import Display from "./components/display/Display.js";
 import DashBoard from "./components/dashBoard/DashBoard.js";
 
 function App() {
+  const [balls, setBalls] = useState(0)
+  const [strikes, setStrikes] = useState(0)
+
   return (
     <div className="App">
     <h1>BaseBall </h1>
-    <Display />
+    <Display balls={balls} strikes={strikes} />
     <DashBoard />
       
     </div>
--- web-app-testing/src/components/display/Display.js
+++ web-app-testing/src/components/display/Display.js
@@ -1,18 +1,20 @@
 import React from "react";
 
+
+
 import './display.css';
 
-function Display() {
-
+function Display(props) {
+	console.log(props);
 	return (
 		<div className="DisplayContainer">
 			<div className="ballsDiv">
 				<h2>Balls</h2>
-				<h2>0</h2>
+				<h2>{props.balls}</h2>
 			</div>
 			<div className="strikesDiv">
 				<h2>Strikes</h2>
-				<h2>0</h2>
+				<h2>{props.strikes}</h2>
 			</div>
 		</div>
 	);
